ebc230b6a40e81ca8388aaa4f49e42b253cdd0c7
Ben Taylor <taylo886@msu.edu>
2014-01-09 21:30:31 -0500

    Updated server to send 200 OK, test header

--- server.py
+++ server.py
@@ -17,7 +17,11 @@ print 'Entering infinite loop; hit CTRL-C to exit'
 while True:
     # Establish connection with client.    
     c, (client_host, client_port) = s.accept()
+    print c.recv(1000)
     print 'Got connection from', client_host, client_port
-    c.send('Thank you for connecting')
-    c.send("good bye.")
+    #c.send('Thank you for connecting')
+    #c.send("good bye.")
+    c.send('HTTP/1.0 200 OK\n')
+    c.send('Content-Type: text/html\n\n')
+    c.send("<h1>Hello, world</h1> this is brtaylor92's Web server\n")
     c.close()
