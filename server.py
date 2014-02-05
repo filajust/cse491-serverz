685a38a923c554ef11c8ba7075ce051626ff58d5
filajust <filajust@msu.edu>
2014-02-05 17:04:01 -0500

    added multipart option

--- server.py
+++ server.py
@@ -8,6 +8,10 @@ def extractPath(input):
     temp = input.splitlines()
     return temp[0].split(' ')[1]
 
+# --------------------------------------------------------------------------------
+#                                 Gets 
+# --------------------------------------------------------------------------------
+
 def index_html(conn):
     conn.send('HTTP/1.0 200 OK\r\n')
     conn.send("Content-type: text/html\r\n\r\n")
@@ -17,14 +21,17 @@ def index_html(conn):
     imageUrl = '/image'
     formGetUrl = '/form'
     formPostUrl = '/formPost'
+    formPostMultipartUrl = '/formPostMultipart'
 
     urls = '<h1>Hello, world.</h1>This is filajust\'s Web server.\r\n\r\n\
             <p><a href="{0}">Content</a></p>\
             <p><a href="{1}">File</a></p>\
             <p><a href="{2}">Image</a></p>\
             <p><a href="{3}">Form</a></p>\
-            <p><a href="{4}">Form (post)</a></p>'.\
-            format(contentUrl, fileUrl, imageUrl, formGetUrl, formPostUrl)
+            <p><a href="{4}">Form (post)</a></p>\
+            <p><a href="{5}">Form (post multipart)</a></p>'.\
+            format(contentUrl, fileUrl, imageUrl, formGetUrl, formPostUrl, 
+                    formPostMultipartUrl)
     conn.send(urls);
 
 def content_html(conn):
@@ -56,18 +63,6 @@ def form_html(conn):
         <input type=\'submit\' value=\'Submit\'>\
         </form>')
 
-def form_post_html(conn):
-    conn.send('HTTP/1.0 200 OK\r\n')
-    conn.send("Content-type: text/html\r\n\r\n")
-
-    conn.send('<p>Please fill in name</p>\
-        <form action=\'/submit\' method=\'POST\' \
-        enctype=\'application/x-www-form-urlencoded\'>\
-        First Name: <input type=\'text\' name=\'firstname\'>\
-        Last Name: <input type=\'text\' name=\'lastname\'>\
-        <input type=\'submit\' value=\'Submit\'>\
-        </form>')
-
 def submit_html(data, conn):
     conn.send('HTTP/1.0 200 OK\r\n')
     conn.send("Content-type: text/html\r\n\r\n")
@@ -80,30 +75,7 @@ def submit_html(data, conn):
         conn.send('<p>Hello Mr. {0} {1}</p>'.format(res\
                 ['firstname'][0], res['lastname'][0]))
 
-def error_html(conn):
-    conn.send('HTTP/1.0 404 Not Found\r\n');
-
-    conn.send('<p>Error</p>')
-    
-def handle_get(path, conn):
-    if path == '/':
-        index_html(conn)
-    elif path == '/content':
-        content_html(conn)
-    elif path == '/file':
-        file_html(conn)
-    elif path == '/image':
-        image_html(conn)
-    elif path == '/form':
-        form_html(conn)
-    elif path == '/formPost':
-        form_post_html(conn)
-    elif path.startswith('/submit'):
-        submit_html(path, conn)
-    else:
-        error_html(conn)
-
-def handle_post(data, conn):
+def urlencoded_html(data, conn):
     conn.send('HTTP/1.0 200 OK\r\n')
     conn.send("Content-type: text/html\r\n\r\n")
 
@@ -119,6 +91,81 @@ def handle_post(data, conn):
         else:
             conn.send('<p>Hello Mr. {0} {1}, thank you for using a post \
                     request</p>'.format(res['firstname'][0], res['lastname'][0]))
+
+def multipart_html(data, conn):
+    conn.send('HTTP/1.0 200 OK\r\n')
+    conn.send("Content-type: text/html\r\n\r\n")
+
+    conn.send('<h1>multipart</h1>')
+    print 'data', data
+
+def error_html(conn):
+    conn.send('HTTP/1.0 404 Not Found\r\n');
+
+    conn.send('<p>Error</p>')
+
+    
+def handle_get(path, conn):
+    if path == '/':
+        index_html(conn)
+    elif path == '/content':
+        content_html(conn)
+    elif path == '/file':
+        file_html(conn)
+    elif path == '/image':
+        image_html(conn)
+    elif path == '/form':
+        form_html(conn)
+    elif path == '/formPost':
+        form_post_html(conn)
+    elif path == '/formPostMultipart':
+        form_post_multipart_html(conn)
+    elif path.startswith('/submit'):
+        submit_html(path, conn)
+    else:
+        error_html(conn)
+
+# --------------------------------------------------------------------------------
+#                                  Posts
+# --------------------------------------------------------------------------------
+
+def form_post_html(conn):
+    conn.send('HTTP/1.0 200 OK\r\n')
+    conn.send("Content-type: text/html\r\n\r\n")
+
+    conn.send('<p>Please fill in name</p>\
+        <form action=\'/submit\' method=\'POST\' \
+        enctype=\'application/x-www-form-urlencoded\'>\
+        First Name: <input type=\'text\' name=\'firstname\'>\
+        Last Name: <input type=\'text\' name=\'lastname\'>\
+        <input type=\'submit\' value=\'Submit\'>\
+        </form>')
+
+def form_post_multipart_html(conn):
+    conn.send('HTTP/1.0 200 OK\r\n')
+    conn.send("Content-type: text/html\r\n\r\n")
+
+    conn.send('<p>Please fill in name</p>\
+        <form action=\'/submit\' method=\'POST\' \
+        enctype=\'multipart/form-data\'>\
+        File: <input type=\'file\' name=\'files\'>\
+        <input type=\'submit\' value=\'Submit\'>\
+        </form>')
+
+def handle_post(data, conn):
+    temp = data.splitlines()[9]
+    content_type = temp.split(' ')[1]
+
+    if content_type == 'application/x-www-form-urlencoded':
+        urlencoded_html(data, conn)
+    elif content_type == 'multipart/form-data;':
+        multipart_html(data, conn)
+    else:
+        error_html(conn);
+
+# --------------------------------------------------------------------------------
+#                           handling the connection 
+# --------------------------------------------------------------------------------
     
 # Send response
 # took some code from 
